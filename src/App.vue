5faafdd914f7450bc68f54d0b10ebf6f354c6a0c
ZakaryaNoori <zakaryanoori979@gmail.com>
2021-01-04 17:25:17 +0430

    Add sidebar component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SideBar/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SideBar from '@/components/SideBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SideBar
   }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav class="sidebar" :class="open ? 'open' : 'closed'">
+    <ul class="sidebar-list">
+      <li class="sidebar-list__item logo">
+        <a href="javascript:void(0)" class="sidebar-list__link">
+          <img src="../assets/images/logo.svg" alt="" width="20">
+          stocklabs
+        </a>
+      </li>
+      <li class="sidebar-list__item search">
+        <div class="expand-icon icon-arrow-right-c" @click="open = true">
+        </div>
+        <a href="javascript:void(0)" class="sidebar-list__link">Search</a>
+      </li>
+      <li class="sidebar-list__item">
+        <a href="javascript:void(0)" class="sidebar-list__link">Home</a>
+      </li>
+      <li class="sidebar-list__item">
+        <a href="javascript:void(0)" class="sidebar-list__link">Market View</a>
+      </li>
+      <li class="sidebar-list__item">
+        <a href="javascript:void(0)" class="sidebar-list__link">News</a>
+      </li>
+      <li class="sidebar-list__item">
+        <a href="javascript:void(0)" class="sidebar-list__link">Industries</a>
+      </li>
+      <li class="sidebar-list__item">
+        <a href="javascript:void(0)" class="sidebar-list__link">Screencer</a>
+      </li>
+      <li class="sidebar-list__item">
+        <a href="javascript:void(0)" class="sidebar-list__link">Watch list</a>
+      </li>
+      <li class="sidebar-list__item">
+        <a href="javascript:void(0)" class="sidebar-list__link">Chat</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'SideBar',
+  data () {
+    return {
+      open: false
+    }
+  }
+}
+</script>
